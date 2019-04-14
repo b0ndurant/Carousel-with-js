9aecc4f810d49ae21dd6e1bd10ce69ab599435a9
b0ndurant <martin.aurelien01@gmail.com>
2019-04-15 00:12:50 +0200

    add function slideShow

---
+++ main.js
@@ -0,0 +1,25 @@
+let slideIndex = 1;
+let interval = setInterval("slidesShow()", 2000);
+
+let slides = document.getElementsByTagName('li');
+let dots = document.getElementsByClassName('dot');
+
+slidesShow();
+
+function slidesShow() {
+
+  (slideIndex > slides.length) ? slideIndex = 1 
+  : (slideIndex < 1) ? slideIndex = 5 
+  : slideIndex;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('active');
+  slideIndex++;
+}
